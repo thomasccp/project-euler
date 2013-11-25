5def50ba2aac4f56ee1034659bcd4bfd662832bf
thomasccp <thomasccp@msn.com>
2013-11-25 01:48:15 +0000

    Tidy up

--- C++/1383161341main2.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "problem2.hpp"
-using namespace std;
-
-int main(){
-	Problem2 p = Problem2();
-	cout << p.
-}
--- C++/main2.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "problem2.hpp"
-using namespace std;
-
-int main(){
-	Problem2 p = Problem2(4000000);
-	cout << p.calculate() << endl;
-}
